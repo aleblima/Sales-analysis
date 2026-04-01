58205a250f7f3d79ce60f6a9f04a7d01a58c74a0
alejandro <alejandrolima1030@gmail.com>
2026-04-01 20:21:04 -0300

    feat: create region entity (closes #4)

---
+++ src/main/java/com/alejandro/sales_analysis/entity/Region.java
@@ -0,0 +1,24 @@
+package com.alejandro.sales_analysis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Regiao")
+public class Region {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "idregiao")
+  private Integer idRegion;
+
+  @Column(name = "nomeregiao")
+  private String regionName;
+}

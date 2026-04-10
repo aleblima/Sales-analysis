b27e1d70ce8e190cb0c8a1f7198c63f3d4f7c1bc
Alejandro <alejandrolima1030@gmail.com>
2026-04-10 00:18:45 -0300

    fix: Region repository Corrigindo a consulta que não era utilizada em RegionRepository

--- backend/src/main/java/com/alejandro/sales_analysis/repository/RegionRepository.java
+++ backend/src/main/java/com/alejandro/sales_analysis/repository/RegionRepository.java
@@ -1,11 +1,10 @@
 package com.alejandro.sales_analysis.repository;
 
 import com.alejandro.sales_analysis.entity.Region;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
-  Optional<Region> findByRegionNameIgnoreCase(String regionName);
+
 }

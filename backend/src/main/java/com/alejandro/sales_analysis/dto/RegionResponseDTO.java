122d84ac27341a1ef220ec10e42e1cc9a0db88fb
alejandro <alejandrolima1030@gmail.com>
2026-04-18 16:24:29 -0300

    fix: Region DTO

--- backend/src/main/java/com/alejandro/sales_analysis/dto/RegionResponseDTO.java
+++ backend/src/main/java/com/alejandro/sales_analysis/dto/RegionResponseDTO.java
@@ -2,7 +2,7 @@ package com.alejandro.sales_analysis.dto;
 
 import com.alejandro.sales_analysis.entity.Region;
 
-public record RegionResponseDTO(Integer id, String nome) {
+public record RegionResponseDTO(Integer id, String name) {
 
   public RegionResponseDTO(Region region) {
     this(region.getIdRegion(), region.getRegionName());

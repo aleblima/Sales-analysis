3bcb4eeea0a2a13b03c03342e99c54772d7624a6
Alejandro <alejandrolima1030@gmail.com>
2026-04-21 03:07:02 -0300

    fix: update SaleResponseDTO

--- backend/src/main/java/com/alejandro/sales_analysis/dto/SaleResponseDTO.java
+++ backend/src/main/java/com/alejandro/sales_analysis/dto/SaleResponseDTO.java
@@ -1,18 +1,7 @@
 package com.alejandro.sales_analysis.dto;
 
-import com.alejandro.sales_analysis.entity.Sale;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record SaleResponseDTO(
-    Integer id, BigDecimal total, LocalDate date, String seller, String region) {
-
-  public SaleResponseDTO(Sale sale) {
-    this(
-        sale.getIdSale(),
-        sale.getTotal(),
-        sale.getDate(),
-        sale.getSellerName(),
-        sale.getRegionName());
-  }
-}
+    Integer id, BigDecimal total, LocalDate date, String seller, String region) {}

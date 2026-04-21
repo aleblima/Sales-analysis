ecaa54b7353967826087b04583299b790cc235b2
Alejandro <alejandrolima1030@gmail.com>
2026-04-20 22:59:45 -0300

    fix: update `SellerRequestDTO`

--- backend/src/main/java/com/alejandro/sales_analysis/dto/SellerRequestDTO.java
+++ backend/src/main/java/com/alejandro/sales_analysis/dto/SellerRequestDTO.java
@@ -3,5 +3,6 @@ package com.alejandro.sales_analysis.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record SellerRequestDTO(@NotBlank(message = "Seller name cannot be blank") String name,
-                               @NotNull(message = "Region cannot be blank") Integer idregion) {}
+public record SellerRequestDTO(
+    @NotBlank(message = "Seller name cannot be blank") String name,
+    @NotNull(message = "Region id cannot be null") Integer idregion) {}

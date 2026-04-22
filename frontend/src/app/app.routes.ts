83798e79e493cd7b0e590a565e85d62e58ae476b
Alejandro <alejandrolima1030@gmail.com>
2026-04-22 00:51:11 -0300

    config: add routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'region',
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./region/region-list/region-list').then(m => m.RegionList)
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('./region/region-form/region-form').then(m => m.RegionForm)
+            }
+        ]
+    },
+    {
+        path: 'seller',
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./seller/seller-list/seller-list').then(m => m.SellerList)
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('./seller/seller-form/seller-form').then(m => m.SellerForm)
+            }
+        ]
+    },
+    {
+        path: 'sale',
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./sale/sale-list/sale-list').then(m => m.SaleList)
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('./sale/sale-form/sale-form').then(m => m.SaleForm)
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: '/region',
+        pathMatch: 'full'
+    },
+];

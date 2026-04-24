c781bd36d6d2eb897a665ca4cf9ddf5c6827a0d5
alejandro <alejandrolima1030@gmail.com>
2026-04-23 21:33:22 -0300

    feat: create seller service

--- frontend/src/app/seller/seller.ts
+++ frontend/src/app/seller/seller.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { apiUrl } from '../../enviroment';
+import { HttpClient } from '@angular/common/http';
+import { Seller } from './seller-module';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Seller {}
+export class SellerService {
+
+  private url = `${apiUrl}/sellers`;
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Seller[]> {
+    return this.http.get<Seller[]>(this.url);
+  }
+
+  create(name: string, regionId: number): Observable<Seller> {
+    return this.http.post<Seller>(this.url, { name, regionId });
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
